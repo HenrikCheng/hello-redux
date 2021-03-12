fa75797d010812c6120b809aaab1532d38d35541
Henrik Cheng <henrikcheng@live.se>
2021-03-12 14:04:44 +0100

    basic redux store setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import HelloWorld from "./HelloWorld";
+import reducer from "./reducers";
+import { createStore } from "redux";
+
+const initialState = { tech: "React " };
+const store = createStore(reducer, initialState);
 
 const App = () => {
-  const [tech] = useState("React");
-  return <HelloWorld tech={tech} />;
+  return <HelloWorld tech={store.getState().tech} />;
 };
 
 export default App;
